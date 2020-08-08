7c43c4fea42b014277ad8744ee66d8636d1e2803
archarajagopalan <69230561+archarajagopalan@users.noreply.github.com>
2020-08-07 21:03:23 -0700

    Add files via upload

---
+++ deep_versions.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 21:21:47 2020
+
+@author: archa
+"""
+
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+1
+2
+3
+4
+5
+6
+7
+8
+9
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
---
+++ keras_first_network.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 22:26:50 2020
+
+@author: archa
+"""
+
